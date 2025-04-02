306b33fcdf36b6a5b87a45fa7ef931a51464ced3
x23062 <x23062xx@aitech.ac.jp>
2025-04-03 00:19:10 +0900

    Add: Skills, Productsページの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/Header/Header.tsx'
+import Header from './pages/Header/Header.tsx'
+import Main from './pages/Main/Main.tsx'
 import Footer from './pages/Footer/Footer.tsx'
-// import { ThreeSample } from './pages/threeSample.tsx'
-// import Water from './pages/water.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    {/* <Water />
-    <ThreeSample /> */}
+    <Header />
+    <Main />
     <Footer />
   </StrictMode>,
 )
---
+++ src/pages/main/Skills/Skills.tsx
@@ -0,0 +1,62 @@
+import './Skills.css';
+import MyIcon from './../../../assets/images/myicon.png';
+
+function Skills() {
+  return (
+    <div className="skills-container">
+      <h2>Skills</h2>
+      <div className="skills-icon">
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+        <div className='skills-peace'>
+          <img src={MyIcon} alt="My Icon" className="profile-image" />
+          <p>text</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
